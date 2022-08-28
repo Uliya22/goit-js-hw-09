a9d1973423a237ccf27620abeee4fe54e0338114
Uliya22 <uliya.d@ukr.net>
2022-08-28 14:31:56 +0300

    Part 3

---
+++ src/js/02-timer.js
@@ -0,0 +1,11 @@
+import flatpickr from 'flatpickr'
+import "flatpickr/dist/flatpickr.min.css";
+
+const inputEl = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]')
+
+startBtn.disabled = true;
+
+startBtn.addEventListener('click', () => {
+    
+})
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+const delayEl  = document.querySelector('input[name="delay"]');
+const stepEl = document.querySelector('input[name="step"]');
+const amountEl = document.querySelector('input[name="amount"]');
+
+form.addEventListener('submit', onFormSubmit);
+
+function  onFormSubmit(evt) {
+  evt.preventDefault();
+  let delay = +delayEl.value;
+  const step = +stepEl.value;
+  const amount = +amountEl.value;
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay).then(Notify.success).catch(Notify.failure);
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }  
+    }, delay);
+  });
 }
